f027941df1a8778205b130dfdeddcb13f17d97ee
Doros Doru-Lucian <doru.doros@paddypowerbetfair.com>
2022-08-18 15:11:28 +0300

    removed node_modules

--- backend/dist/config/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SECRET = exports.IS_PROD = void 0;
-exports.IS_PROD = process.env.NODE_ENV === 'production';
-exports.SECRET = exports.IS_PROD ? process.env.SECRET : 'secret';
